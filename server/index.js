0403ea3448946e72681ed520ac87cbb7a05f03e9
Greta Schock <gschockcodes@gmail.com>
2023-04-07 11:14:51 -0700

    Created a successful api with cluebase

--- server/index.js
+++ server/index.js
@@ -11,6 +11,22 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use(express.static(PUBLIC_DIR));
 
+app.get('/api/jeopartytest', (req, res) => {
+  // axios.get(`https://developer.nps.gov/api/v1/parks?limit=271&q=hiking&api_key=${API_KEY_NP}`)
+  // axios.get(`https://restcountries.com/v3.1/all?fields=name,flags`)
+  // axios.get(`https://cluebase.io/api/categories?&count=1'`)
+  axios.get(`http://cluebase.lukelav.in/clues/random`)
+  .then((results) => {
+      console.log(results)
+      res.send(results.data.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send(500);
+    })
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
   })
+
