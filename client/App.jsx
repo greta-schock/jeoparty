0a3677743412b3087280891607268dfa5b84e080
Greta Schock <gschockcodes@gmail.com>
2023-04-07 10:05:43 -0700

    Added a header and a by line for project (placeholder)

--- client/App.jsx
+++ client/App.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 const App = () => {
     return (
-        <div>Is this thing on?</div>
+        <div>
+        <h1>This is Jeoparty!</h1>
+        <div>Welcome to Jeoparty, an interactive game where you get to practice previous Jeoparty questions. I'm your host, Alex Trebek.</div>
+        </div>
+            
     )
 }
 
